32e472cf933749c913ca1b719c9112505b904d7f
boss683 <adamhuseinhussein27@gmail.com>
2025-11-04 08:22:35 +0200

    work porject chat

---
+++ hussein.js
@@ -0,0 +1,11 @@
+var herf=location.herf;
+var  get = "two"
+document .write(get)
+let ss=34;
+let gh=34;
+console.log(34+34);
+var pvs='<p>thes code dos not you need worke withe me<p>'
+document.getElementById('red')
+document.write(red);
+red.getElementById(red)
+
